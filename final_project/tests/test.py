a9e599c11a0f5330e4dca7be96fa29933cfc4c11
davidsanzmunoz <103991201+davidsanzmunoz@users.noreply.github.com>
2022-04-20 00:26:05 +0200

    Final update

--- final_project/tests/test.py
+++ final_project/tests/test.py
@@ -1,20 +1,20 @@
 import unittest
 
-from machinetranslation.translator import french_to_english, english_to_french
+#from translator import french_to_english, english_to_french
+from translator import english_to_french, french_to_english
 
+class TestTranslator(unittest.TestCase):
+    def test_e2f_null(self):
+        self.assertNotEqual(english_to_french("None"), '')
+    def test_e2f_notequal(self):
+        self.assertNotEqual(english_to_french(0), 0)
+    def test_e2f_translation(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+    def test_f2e_null(self):
+        self.assertNotEqual(french_to_english("None"), '')
+    def test_f2e_notequal(self):
+        self.assertNotEqual(french_to_english(0), 0)
+    def test_f2e_translation(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
-class TestTranslator(unittest.TestCase): 
-
-    def test_f2e_null(self): 
-        self.assertEqual(french_to_english(), None)  # Test F2E Null
-
-    def test_f2e_notnull(self): 
-        self.assertEqual(french_to_english("Bonjour"),"Hello")  # Test F2E Word
-
-    def test_e2f_null(self): 
-        self.assertEqual(english_to_french(), None)  # Test E2F Null
-
-    def test_e2f_notnull(self): 
-        self.assertEqual(english_to_french("Hello"),"Bonjour")  # Test E2F Word
-
-
+unittest.main()    	
