360078471ba1146eb701ac97acf085346190e6b1
davidsanzmunoz <103991201+davidsanzmunoz@users.noreply.github.com>
2022-04-20 00:26:41 +0200

    Final update

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,13 +8,13 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('apikey')
+authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version='0.1',
+    version='2022-04-19',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com')
+language_translator.set_service_url(url)
 
 
 def french_to_english(frenchtext):
@@ -24,7 +24,7 @@ def french_to_english(frenchtext):
     englishtext = language_translator.translate(
         text=frenchtext,
         model_id='fr-en').get_result()
-    return englishtext.get('translations')[0].get('translations')
+    return englishtext.get('translations')[0].get('translation')
 
 
 
@@ -35,6 +35,5 @@ def english_to_french(englishtext):
     frenchtext = language_translator.translate(
         text=englishtext,
         model_id='en-fr').get_result()
-    return frenchtext.get('translations')[0].get('translations')
-
+    return frenchtext.get('translations')[0].get('translation')
 
